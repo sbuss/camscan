0e4f71cea6ef38f235ae26fdc7f2c313613f683b
Steven Buss <sbuss@counsyl.com>
2014-04-13 17:57:28 -0700

    Move video init to camscan.js

---
+++ camscan.js
@@ -0,0 +1,35 @@
+/* CamScan */
+
+/* Bind the webcam to a <video> element.
+ *
+ * Args:
+ *   `element_name` The name of the <video> element to bind to
+ * Returns a reference to the bound element.
+ */
+function bindCameraToElement(element_name) {
+  var video = $(element_name)[0],
+      videoObj = { "video": true, "audio": false },
+      errBack = function(error) {
+          console.log("Video capture error: ", error); 
+      };
+
+  // Put video listeners into place
+  if(navigator.getUserMedia) { // Standard
+      navigator.getUserMedia(videoObj, function(stream) {
+          video.src = stream;
+          video.play();
+      }, errBack);
+  } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+      navigator.webkitGetUserMedia(videoObj, function(stream){
+          video.src = window.webkitURL.createObjectURL(stream);
+          video.play();
+      }, errBack);
+  }
+  else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+      navigator.mozGetUserMedia(videoObj, function(stream){
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      }, errBack);
+  }
+  return video;
+}
